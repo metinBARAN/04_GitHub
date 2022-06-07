cabc2644bf893ecaf203578b05a3ba266e0eaae1
metinBARAN <metinbaran0000@gmail.com>
2022-06-07 20:47:01 +0300

    Bu benim ilk gonderim

---
+++ 04_GitHub/src/main/java/com/metinbaran/App.java
@@ -0,0 +1,23 @@
+package com.metinbaran;
+
+/**
+ * Hello world!
+ *
+ */
+public class App {
+
+    private static void selamla(){
+    	System.out.println( "Selam GitHub" );
+	
+}
+
+    public static void main( String[] args )
+    {
+    	
+    	Islem islem = new Islem();
+    	islem.goster();
+    	
+    	selamla();
+        
+    }
+}
---
+++ 04_GitHub/src/main/java/com/metinbaran/Islem.java
@@ -0,0 +1,11 @@
+package com.metinbaran;
+
+public class Islem {
+	
+	int veriler[] = {1,2,3};
+	
+	public void goster() {
+		System.out.println("Sonuçları Göster...");
+	}
+
+}
